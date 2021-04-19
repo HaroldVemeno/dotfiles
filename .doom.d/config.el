fbb7501bb996538ba2bd06a05e57cafb5c92d83f
Miky <haroldvemeno@email.cz>
2021-04-19 11:48:17 +0200

    emmet

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -60,3 +60,6 @@
 (elcord-mode)
 
 (after! cc-mode (set-ligatures! '(c++-mode) nil))
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
