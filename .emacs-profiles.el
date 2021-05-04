d6865fc80f78a6d29575e8ad6f9aa475b7fdcd33
Miky <haroldvemeno@email.cz>
2021-05-04 15:28:15 +0200

    Spacemacs ??

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,3 @@
 (("default" . (( user-emacs-directory . "~/.emacs.default")))
+ ("spacemacs" . (( user-emacs-directory . "~/.emacs.spacemacs")))
  ("doom" . (( user-emacs-directory . "~/.emacs.doom"))))
