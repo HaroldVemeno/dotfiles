c622b9fef07b519d57a446119a7ea364a5403e6c
Miky <haroldvemeno@email.cz>
2021-05-20 23:54:41 +0200

    Moving .doom.d to .config/doom

---
+++ .config/chemacs/profiles.el
@@ -0,0 +1,3 @@
+(("default" . (( user-emacs-directory . "~/.config/emacs-default")))
+ ("spacemacs" . (( user-emacs-directory . "~/.config/emacs-spacemacs")))
+ ("doom" . (( user-emacs-directory . "~/.config/emacs-doom"))))
