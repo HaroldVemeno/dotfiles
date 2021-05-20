4756e94cb07fe790d12e05e170e05533e7841378
Miky <haroldvemeno@email.cz>
2021-05-21 00:00:50 +0200

    Doom config relocation

--- .emacs-profiles.el
+++
@@ -1,3 +0,0 @@
-(("default" . (( user-emacs-directory . "~/.emacs.default")))
- ("spacemacs" . (( user-emacs-directory . "~/.emacs.spacemacs")))
- ("doom" . (( user-emacs-directory . "~/.emacs.doom"))))
